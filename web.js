50fa4d15994c3bd867e46a0e64b7fb5bf08f4bdc
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2011-12-30 20:22:08 -0500

    configured socket.io for long poll

--- web.js
+++ web.js
@@ -7,6 +7,11 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
