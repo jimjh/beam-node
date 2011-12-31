8df779be8708a531793fc82829bb6ef218631c03
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2011-12-30 23:07:42 -0500

    added delete request on disconnect

--- web.js
+++ web.js
@@ -1,4 +1,12 @@
+/**
+ * Web Socket server
+ */
+
+//-------------------------------------------------
+// App bootstrapping
+//-------------------------------------------------
 var app = require('express').createServer()
+  , http = require('http')
   , io = require('socket.io').listen(app);
 
 var port = process.env.PORT || 3000;
@@ -10,13 +18,35 @@ app.get('/', function (req, res) {
   res.send("Hello, World!");
 });
 
+//------------------------------------------------
+// Socket.IO setup
+//------------------------------------------------
+
+var KEY_UUID = 'uuid';
+var EVT_SET_UUID = 'set uuid';
+var APP_HOST = "afternoon-fire-7441.heroku.com;
+
+/* 
+ * heroku doesn't support websockets yet, so we
+ * fall back to polling
+ */
 io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
   io.set("polling duration", 10); 
 });
 
 io.sockets.on('connection', function (socket) {
-  socket.on('set uuid', function (uuid) {
-    console.log(uuid);
+  // set endpoint UUID for this session
+  socket.on(EVT_SET_UUID, function (uuid) {
+    socket.set(KEY_UUID, uuid);
+  });
+  // on disconnect, tell app server
+  socket.on('disconnect', function () {
+    http.request({
+      host: APP_HOST,
+      method: 'DELETE',
+      path: '/endpoints/' + socket.get(KEY_UUID),
+      auth: 'codex:abc'
+    }, function () {});
   });
 });
