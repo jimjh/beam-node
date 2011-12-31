ceea7ff3497e7a65efe87cbf5dd13a1034d426de
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2011-12-30 20:21:25 -0500

    express + socket.io

--- web.js
+++ web.js
@@ -1,4 +1,11 @@
-var io = require('socket.io').listen(80);
+var app = require('express').createServer()
+  , io = require('socket.io').listen(app);
+
+app.listen(80);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
