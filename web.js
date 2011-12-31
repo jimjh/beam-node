6cb7da9c1ad7cbd4eb717adb08bf3011ab142769
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2011-12-31 15:49:02 -0500

    reorganized code a little

--- web.js
+++ web.js
@@ -1,23 +1,27 @@
 /**
  * Web Socket server
- * Notifies app server when a client is disconnected.
+ * * Registers endpoint UUID for each connected client
+ * * Notifies app server when a client is disconnected
  * @author Jiunn Haur Lim
  */
+
+var app = require('express').createServer();
+var http = require('http');
+var io = require('socket.io').listen(app);
 
 //-------------------------------------------------
 // App bootstrapping
 //-------------------------------------------------
-var app = require('express').createServer()
-  , http = require('http')
-  , io = require('socket.io').listen(app);
 
+// copied from heroku example
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
-  console.log("Listening on port " + port);
+  console.log('Listening on port ' + port);
 });
 
+// reply with "Hello, World!" for every request
 app.get('/', function (req, res) {
-  res.send("Hello, World!");
+  res.send('Hello, World!');
 });
 
 //-------------------------------------------------
@@ -26,7 +30,7 @@ app.get('/', function (req, res) {
 
 var KEY_UUID = 'uuid';
 var EVT_SET_UUID = 'set uuid';
-var APP_HOST = "afternoon-fire-7441.heroku.com";
+var APP_HOST = 'afternoon-fire-7441.heroku.com';
 
 /* 
  * heroku doesn't support websockets yet, so we
@@ -37,44 +41,65 @@ io.configure(function () {
   io.set("polling duration", 10); 
 });
 
+/**
+ * Unregisters an endpoint with the given UUID from the
+ * app server.
+ * @param uuid      UUID of endpoint to unregister
+ */
 var unregister_endpoint = function (uuid){
 
-  var req =  http.request({
+  // DELETE /endpoints/<uuid> HTTP/1.1
+  var http_options = {
     host: APP_HOST,
     hostname: APP_HOST,
     method: 'DELETE',
     path: '/endpoints/' + uuid,
-    headers : {'Content-Length' : 0}
-  }, function(res){
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-      console.log('BODY: ' + chunk);
-    });
-  });
+    headers : {'Content-Length' : 0},
+    auth: "codex:abc"
+  };
 
-  req.on('error', function(e){
-    console.log('Problem with request: ' + e.message);
-  });
+  // invoked on response from app server
+  var response_callback = function (res){
+    console.log('Status from app server: ' + res.statusCode);
+  };
 
+  // invoked on connection error
+  var error_handler = function (e){
+    console.log ('Problem with request: ' + e.message);
+  };
+
+  // fire http request
+  var req =  http.request(http_options, response_callback);
+  req.on('error', error_handler);
   req.end();
 
-}
+};
 
-io.sockets.on('connection', function (socket) {
-
+/**
+ * Registers UUID for an endpoint and listens for the
+ * 'disconnect' event.
+ * @param socket
+ * @param uuid        UUID of endpoint to register
+ */
+var register_endpoint = function (socket, uuid){
+  
   // set endpoint UUID for this session
-  socket.on(EVT_SET_UUID, function (uuid) {
-    socket.set(KEY_UUID, uuid);
-  });
+  socket.set(KEY_UUID, uuid);
 
   // on disconnect, tell app server
   socket.on('disconnect', function () {
-    console.log ("disconnect received.");
+    console.log ("Disconnect received.");
     socket.get(KEY_UUID, function (err, uuid) {
       unregister_endpoint(uuid);
     });
   });
 
+};
+
+// listen for incoming connections
+io.sockets.on('connection', function (socket) {
+  // listen for "set_uuid" event
+  socket.on(EVT_SET_UUID, function (uuid) {
+    register_endpoint(socket, uuid);
+  });
 });
