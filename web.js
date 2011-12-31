f2464892975e887dfe78290237d1321baf1696ee
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2011-12-30 19:40:03 -0500

    first commit

---
+++ web.js
@@ -0,0 +1,12 @@
+var s = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});equire.paths.push('/opt/local/lib/node_modules');
